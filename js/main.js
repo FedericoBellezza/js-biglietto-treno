2c1923309b0059152e6d596d51b61d0ff2f6214b
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-14 14:22:18 +0200

    add data collection

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 - va applicato uno sconto del 40% per gli over 65.
 - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
+
+// & RACCOLTA DATI
+// ^ DATI DALL'UTENTE
+let userKm = parseInt(prompt('Quanti chilomentri dovrai percorrere?'));
+let userAge = parseInt(prompt('Inserisci la tua età'));
+
+// ^ DATI A DISPOSIZIONE
+let indexPricePerKm = 0.21;
+let over18Discount = 0.2;
+let over65Discount = 0.4;
+
+let finalPrice;
